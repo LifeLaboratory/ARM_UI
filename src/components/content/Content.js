857132eaa1f7a227d7d60a5c1987e371e19e0f1d
pshenmic <pshenmic@gmail.com>
2018-10-06 01:28:48 +0700

    inject templates into react

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const Content = () => (
-    <div>
-        Main Content
+    <div className="content-container">
+
+        <div className="chat">
+            <div className="chat__message-field">
+            <textarea name="" id="" cols="30" rows="10">
+                MESSAGE
+            </textarea>
+            </div>
+
+            <div className="chat__send-btn">Отправить</div>
+        </div>
+
     </div>
 );
 
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-const Sidebar = () => (
-    <div>
-        SidebarComponent
-    </div>
-);
+class Sidebar extends React.Component {
+    render() {
+        return(
+            <div className="sidebar">
+                <div className="operator-info sidebar__operator-info">
+                    <div className="operator-info__avatar-wrapper">
+                        <img src="http://www.master-hard.com/wp-content/uploads/2008/08/avatar-thumb.jpg" alt=""
+                             className="operator-info__avatar-img"/>
+                    </div>
+
+                    <div className="operator-info__name">Иван Василичъ</div>
+
+                    <div className="operator-info__link">Профиль</div>
+
+                    <div className="operator-info__status active">
+                        В работе
+                    </div>
+
+                    <div className="operator-info__link">Выход</div>
+                </div>
+
+                <div className="sidebar__items-list">
+                    <a href="" className="sidebar__item">Чаты</a>
+                    <a href="" className="sidebar__item">Статистика</a>
+                </div>
+            </div>
+        )
+    }
+
+}
 
 export default Sidebar
