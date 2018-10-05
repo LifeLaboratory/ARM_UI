5eac85169422f939b0f82ebb821167ca5b494d12
pshenmic <pshenmic@gmail.com>
2018-10-06 03:57:38 +0700

    Sidebar refactor

---
+++ src/components/sidebar/SidebarLinkItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+class SidebarLinkItem extends React.Component {
+    render() {
+        const {description} = this.props;
+
+        return (
+            <a href="" className="sidebar__item">{description}</a>
+        )
+    }
+}
+
+SidebarLinkItem.propTypes = {
+    description: PropTypes.string
+};
+
+export default SidebarLinkItem
